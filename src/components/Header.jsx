ad06b332128371e4920b4a823e65cebc0313039f
Hamza <88616464+muhammadhamza2001@users.noreply.github.com>
2023-10-26 17:15:08 +0500

    Header is added

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Menu } from 'antd';
+import { HomeOutlined, AppstoreAddOutlined, UserOutlined, SettingOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
+const TopNavbar = () => {
+    return (
+        <Menu mode="horizontal" theme="dark">
+            <Menu.Item key="logo" style={{ display: 'flex', alignItems: 'center' }}>
+                {/* <img
+                    src="/path-to-your-logo.png" 
+                    alt="Logo"
+                    style={{ height: 30, width: 30, marginRight: 10 }}
+                /> */}
+                Your Company Name
+            </Menu.Item>
+            <Menu.Item key="home" icon={<HomeOutlined />}>
+                <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item key="about" icon={<AppstoreAddOutlined />}>
+            <Link to="/about">About</Link>
+            </Menu.Item>
+            
+            
+
+        </Menu>
+    );
+};
+
+export default TopNavbar;
